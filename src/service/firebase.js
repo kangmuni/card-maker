c4318a23db58dc6948922615d2d384d7d7b7c058
Muni <kangmoonee@gmail.com>
2021-05-01 18:01:03 +0900

    add login

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -7,4 +8,6 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp;
