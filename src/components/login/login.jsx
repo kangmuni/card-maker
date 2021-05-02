cea415ba4e6ad844c831f4893bf01c0e40a31a40
Muni <kangmoonee@gmail.com>
2021-05-02 15:07:17 +0900

    add login and logout functionality And add editor and preview layout

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: '/maker',
+      state: { id: userId },
+    });
+  };
+
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log);
+      .then((data) => goToMaker(data.user.uid));
   };
+
+  useEffect(() => {
+    authService //
+      .onAuthChange((user) => {
+        user && goToMaker(user.uid);
+      });
+  });
+  // useEffect가 실행될때 onAuthChange안의 콜백함수가 실행 되는것이 아닌
+  // 콜백함수의 등록이 이루어지고 실제 콜백함수의 작동은 authChange 이벤트가 생겼을때 일어난다.
+
   return (
     <section className={styles.login}>
       <Header />
---
+++ src/components/maker/maker.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
+import styles from './maker.module.css';
+import { useHistory } from 'react-router-dom';
+
+const Maker = ({ authService }) => {
+  const history = useHistory();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService //
+      .onAuthChange((user) => {
+        if (!user) {
+          history.push('/');
+        }
+      });
+  });
+
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
