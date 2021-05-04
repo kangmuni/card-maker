ec0f97dfc1e7bdb725e1a3e5866af9ce3b698d28
Muni <kangmoonee@gmail.com>
2021-05-04 09:41:00 +0900

    handle image uploading

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,7 +6,7 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 import { useHistory } from 'react-router-dom';
 
-const Maker = ({ authService }) => {
+const Maker = ({ FileInput, authService }) => {
   const [cards, setCards] = useState({
     1: {
       id: '1',
@@ -80,6 +80,7 @@ const Maker = ({ authService }) => {
       <Header onLogout={onLogout} />
       <div className={styles.container}>
         <Editor
+          FileInput={FileInput}
           cards={cards}
           addCard={createOrUpdateCard}
           updateCard={createOrUpdateCard}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.module.css';
 import App from './app';
 import AuthService from './service/auth_service';
+import ImageUploader from './service/image_uploader';
+import ImageFileInput from './components/image_file_input/image_file_input';
 
 const authService = new AuthService();
+const imageUploader = new ImageUploader();
+const FileInput = (props) => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+); // 확장을 위한 이 컴포넌트 자체를 어플리케이션이나 다른 컴포넌트로 전달 해줄것임! dependency injection
+
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} />
+    <App authService={authService} FileInput={FileInput} />
   </React.StrictMode>,
   document.getElementById('root')
 );
